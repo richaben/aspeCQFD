4102cc3d08a43d4310f01ae7ccb934fbe09dc9c5
richaben <brichard.rb@gmail.com>
2024-09-19 09:46:21 +0200

    ajout fonction 🆕

--- R/utils.R
+++ R/utils.R
@@ -13,3 +13,33 @@ integer_breaks <- function(n = 5, ...) {
   }
   return(fxn)
 }
+
+#' Colour scheme for ipr plot
+#'
+#' @noRd
+#' 
+#'
+col_ipr <- setNames(c("#1f78b4","#b2df8a","#ffff33","#ff7f00","#e41a1c"),
+                    c("Tr\u00e8s bon",'Bon', 'Moyen', 'M\u00e9diocre', 'Mauvais'))
+
+#' Color table for ipr plot
+#'
+#' @noRd
+#' 
+#'
+col_ipr_classe <- function(){
+  classe_ipr %>% 
+  dplyr::filter(cli_altitude_min != 500 | is.na(cli_altitude_min) ) %>% 
+  dplyr::select(cli_classe,cli_libelle,cli_borne_inf,cli_borne_sup) %>% 
+  unique() %>% 
+  dplyr::mutate(col_ipr_classe = dplyr::recode(cli_libelle, !!!col_ipr),
+                cli_libelle = factor(cli_libelle, 
+                                     ordered = T, 
+                                     levels= c("Tr\u00e8s bon",
+                                               "Bon",
+                                               "Moyen",
+                                               "M\u00e9diocre",
+                                               "Mauvais"))) %>% 
+  dplyr::mutate(cli_borne_sup = dplyr::case_when(cli_borne_sup == 99 ~ 60,
+                                                 TRUE ~ cli_borne_sup))
+}
